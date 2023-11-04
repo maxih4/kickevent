771ee7e189dbf0f34afa9f30986249878f985faa
Max Handke <handkemax@gmail.com>
2023-11-04 12:39:52 +0100

    first commit

---
+++ src/main/java/com/example/kickevent/KickeventApplication.java
@@ -0,0 +1,13 @@
+package com.example.kickevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KickeventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KickeventApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kickevent/KickeventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kickevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KickeventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
