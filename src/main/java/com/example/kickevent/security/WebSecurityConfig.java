c8ac791c919e9d97dee992a10737f4b061500a83
Max Handke <handkemax@gmail.com>
2024-01-12 16:00:21 +0100

    added paging

--- src/main/java/com/example/kickevent/security/WebSecurityConfig.java
+++ src/main/java/com/example/kickevent/security/WebSecurityConfig.java
@@ -66,7 +66,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 // dont authenticate this particular request
                 .authorizeRequests().requestMatchers(CorsUtils::isCorsRequest).permitAll()
                 .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-                .antMatchers("/login", "/register", "/refreshtoken", "/api/event","/user","/error").permitAll().
+                .antMatchers("/login", "/register", "/refreshtoken", "/api/event","/user","/error,","/api/event*").permitAll().
                 // all other requests need to be authenticated
                         anyRequest().authenticated().and().
                 // make sure we use stateless session; session won't be used to
