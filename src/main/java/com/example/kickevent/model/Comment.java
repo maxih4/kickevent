9691e1dfed56d51708f38e23ce388b9c46086aa9
Max <Max>
2026-08-23 01:22:24 +0200

    fix(backend): accept new comments without an id

--- src/main/java/com/example/kickevent/model/Comment.java
+++ src/main/java/com/example/kickevent/model/Comment.java
@@ -20,7 +20,7 @@ public class Comment {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
     @ManyToOne(fetch = FetchType.EAGER, optional = false)
     @JoinColumn(name = "event_id", nullable = false)
